f237b2e045dad19ece8def77f5b5b85a5895bc68
Andrey Stahl <andrey.stahl444@gmail.com>
2023-09-04 16:54:11 -0300

    novos exercícios

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const entrada = document.querySelector(".paragraph-js")
+// const entrada = document.querySelector(".paragraph-js")
 
-entrada.textContent= "novo texto"
-console.log(entrada.textContent) //so html 
-console.log(entrada.innerText) // considera o css
-console.log(entrada.innerHTML) // trás tudo
+// entrada.textContent= "novo texto"
+// console.log(entrada.textContent) //so html 
+// console.log(entrada.innerText) // considera o css
+// console.log(entrada.innerHTML) // trás tudo (mais usado)
+
+button.style.BackgroundColor = "#ffffff" 
+
+function cliqueiNoBotao(){ 
+    alert("Cliquei no botão")
+}
